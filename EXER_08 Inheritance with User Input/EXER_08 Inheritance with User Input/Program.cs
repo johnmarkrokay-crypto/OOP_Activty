f9e8054889b0ba3273b1a9a518ad37553e0e5874
ACT-STUDENT <ACT-STUDENT@LAB4-PC40>
2025-09-11 17:00:11 +0800

    exer8

---
+++ EXER_08 Inheritance with User Input/EXER_08 Inheritance with User Input/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXER_08_Inheritance_with_User_Input
+{
+    internal class Program
+    {
+        //Name: John Mark R. Okay                     Section: IT306
+        //Title:EXER_08: Inheritance with User Input
+
+        static void Main(string[] args)
+        {
+            //User Input for Jersey or Sleevless
+            Console.WriteLine("Enter the Shirt Details:");
+            Console.WriteLine("Jersey or Sleeveless");
+            Console.Write("Enter your choice(Jersey or Sleeveless):\t");
+            string choice = Console.ReadLine();
+
+
+            if (choice == "Jersey")
+            {
+                //Initiate object for Jersey
+                Jersey jersey = new Jersey();
+
+                //User Input fot Jersey
+                Console.Write("\nEnter the Size    :");
+                jersey.Size = Console.ReadLine();
+                Console.Write("Enter the Brand   :");
+                jersey.Brand = Console.ReadLine();
+                Console.Write("Enter the Material:");
+                jersey.Material = Console.ReadLine();
+                Console.Write("Enter the Price   :");
+                jersey.Price = Convert.ToInt32(Console.ReadLine());
+
+                //Call method to display Jersey Information
+                jersey.JerseyInfo();
+            }
+            else if (choice == "Sleeveless")
+            {
+                //Initaite object for Sleeveless
+                Sleeveless sleeveless = new Sleeveless();
+
+                //User Input for Sleeveless
+                Console.Write("\nEnter the Size    :");
+                sleeveless.Size = Console.ReadLine();
+                Console.Write("Enter the Brand   :");
+                sleeveless.Brand = Console.ReadLine();
+                Console.Write("Enter the Quantity:");
+                sleeveless.Quantity = Console.ReadLine();
+                Console.Write("Enter the Color   :");
+                sleeveless.Color = Console.ReadLine();
+
+                //Call method to display Sleeveless Information
+                sleeveless.SleevelessInfo();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ EXER_08 Inheritance with User Input/EXER_08 Inheritance with User Input/Shirt.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXER_08_Inheritance_with_User_Input
+{
+    internal class Shirt // Base Class
+    {
+        //Fields
+        private string size; 
+        private string brand;
+
+        //Properties
+        public string Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+        //Properties
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+        //Methods to display
+        public void ShirtInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nShirt Details:");
+            Console.WriteLine("Size              :" + size);
+            Console.WriteLine("Brand             :" + brand);
+        }
+    }
+
+    class Jersey : Shirt // Derived Class (child)
+    {
+        //Fields
+        private string material;
+        private int price;
+
+        //Public property for Material
+        public string Material
+        {
+            get { return material; }
+            set { material = value; }
+        }
+        //Public property for Price
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        //Method to display for Derived class
+        public void JerseyInfo()
+        {
+            ShirtInfo();
+            Console.WriteLine("Materials used    :" + material);
+            Console.WriteLine("Price             :" + price);
+        }
+    }
+    class Sleeveless : Shirt //Another Derived class (child)
+    {
+        //Fields
+        private string quantity;
+        private string color;
+
+        //Public property for Quantity
+        public string Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+        //Public property for Color
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        //Method to display for Derived class
+        public void SleevelessInfo()
+        {
+            ShirtInfo();
+            Console.WriteLine("Quantity           :" + quantity);
+            Console.WriteLine("Color              :" + color);
+        }
+    }
+}
