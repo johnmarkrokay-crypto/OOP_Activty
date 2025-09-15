985f4a30d75440f70d4a5c47022d5063b45bdd1d
ACT-STUDENT <ACT-STUDENT@LAB3-PC30>
2025-09-15 14:52:21 +0800

    Calculation

---
+++ CalculationPractice/CalculationPractice/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tWhat do you want to buy?, \n\n\t100 DISCOUNT, \n\n\tAnd also have a discount if you have an any ID");
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("\nDo you have an any ID?\t\t");
+            Console.Write("\tJersey 250     and     Sleeveless 150:\t\t");
+            string choice = Console.ReadLine();
+
+            if (choice == "Jersey")
+            {
+                Shirt jersey = new Jersey(250, 100, 50);
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n\tJersey Price Discounted:\t" + jersey.PriceDiscount);
+            }
+            else if (choice == "Sleeveless")
+            {
+                Shirt sleeveless = new Sleeveless(150, 100, 50);
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"\n\tSleeveless Price Discounted:\t" + sleeveless.PriceDiscount);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CalculationPractice/CalculationPractice/Shirt.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationPractice
+{
+    internal class Shirt
+    {
+        public double Price { get; set; }
+
+        public double Discount { get; set; }
+
+        public double Id { get; set; }
+
+        public virtual double PriceDiscount
+        {
+            get { return Price - Discount; }
+        }
+
+        public virtual double IdDiscount
+        {
+            get { return PriceDiscount - Id; }
+        }
+    }
+
+    class Jersey : Shirt
+    {
+        public override double PriceDiscount
+        {
+            get { return Price - Discount; }
+        }
+
+
+        public Jersey(double aPrice, double aDiscount, double aId)
+        {
+            Price = aPrice;
+            Discount = aDiscount;
+            Id = aId;
+        }
+    }
+
+    class Sleeveless : Shirt
+    {
+        public override double PriceDiscount
+        {
+            get { return Price - Discount; }
+        }
+        public Sleeveless(double aPrice, double aDiscount, double aId)
+        {
+            Price = aPrice;
+            Discount = aDiscount;
+            Id = aId;
+        }
+    }
+}
